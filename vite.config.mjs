2402be3f32d5d63445952b9fd293bc5688e48342
tyange <usun16@gmail.com>
2024-02-11 09:36:37 +0900

    vercel 배포 준비

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,11 @@
 import { defineConfig } from "@solidjs/start/config";
+import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    solid({
+      adapter: vercel({ edge: true }),
+    }),
+  ],
+});
