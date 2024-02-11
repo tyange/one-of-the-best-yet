8b50adf8327ab63e5a3e4d249dd3cb2c35174ca3
tyange <usun16@gmail.com>
2024-02-11 10:27:53 +0900

    title, favicon 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,14 @@
-import { A } from "@solidjs/router";
+import { MetaProvider, Title } from "@solidjs/meta";
+
 import MainGrid from "~/components/MainGrid/MainGrid";
 
 export default function Home() {
-  return <MainGrid />;
+  return (
+    <>
+      <MetaProvider>
+        <Title>태양의 온라인 이력서 | one of the best yet</Title>
+      </MetaProvider>
+      <MainGrid />
+    </>
+  );
 }
